1054ad868a8271c645d3f8be0cdb2413d97df950
Daniflav94 <danyelly_mell@hotmail.com>
2023-04-28 17:43:27 -0300

    Create post finalizado, home semi-pronta, hook para puxar dados do firebase concluído

---
+++ miniblog/src/hooks/useFetchDocuments.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+  QuerySnapshot,
+} from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) return;
+      setLoading(true);
+
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        let q;
+        // vai buscar os registros pela ordem dos mais recentes
+        q = await query(collectionRef, orderBy("createdAt", "desc"));
+
+        //vai mapear os dados, se houver alteração vai atualizar
+        await onSnapshot(q, (QuerySnapshot) => {
+          setDocuments(
+            QuerySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+
+        setLoading(false);
+      }
+    }
+
+    loadData();
+  }, [docCollection, search, uid, cancelled]); // só vai ser executada quando um desses dados sofrer alterações
+
+
+  useEffect(() => { //faz limpeza de memória
+    return () => setCancelled(true);
+  }, []);
+
+
+  return {documents, loading, error} //retorna como obj para poder acessar os itens individualmente
+};
